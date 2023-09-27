623bf46eae754cf4d953f6406c8ee4297c5a8171
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-09-27 19:01:40 +0530

    fetching question details from database

--- controllers/api/v1/questions_controller.js
+++ controllers/api/v1/questions_controller.js
@@ -41,3 +41,38 @@ module.exports.createQuestion = async function(req, res){
         });
     }
 }
+
+// action to view a question and it's options
+module.exports.getQuestion = async function(req, res){
+    try{
+        // find the question
+        const question = await Question.findById(req.params.id);
+
+        // if question exists
+        if(question){
+            // enable question record to access it's options data
+            await question.populate({path: 'options', select: '_id value votes link_to_vote'});
+
+            console.log(`Question ${question._id} fetched successfully`);
+            return res.status(200).json({
+                message: `Question ${question._id} fetched successfully`,
+                success: true,
+                data: {
+                    question
+                }
+            });
+        }else{
+            console.log(`Question ${question._id} does not exists`);
+            return res.status(404).json({
+                message: `Question ${question._id} does not exists`,
+                success: false,
+            }); 
+        }
+    }catch(err){
+        console.log(`Error: ${err}`);
+        return res.status(500).json({
+            message: 'Internal server error',
+            success: false,
+        }); 
+    }
+}
--- routes/api/v1/questions.js
+++ routes/api/v1/questions.js
@@ -10,6 +10,7 @@ const router = express.Router(); //create the express router
 // route the requests
 router.post('/create', questionsController.createQuestion); //route the request to createQuestion action of questions_controller
 router.post('/:id/options/create', optionsController.createOption); //route the request to createOption action of optiond_controller
+router.get('/:id', questionsController.getQuestion); //route the request to getQuestion action of questions_controller
 
 // export the router
 module.exports = router;
