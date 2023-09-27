492ea90d41c9280bc82e95be10850d0d2f25d689
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-09-27 11:22:06 +0530

    Setting up express server

---
+++ index.js
@@ -0,0 +1,16 @@
+// imports
+const express = require('express'); //import the express module
+
+const app = express(); //create the express app
+const port = 8000; //define the port
+
+
+// listen on the server
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+        return;
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
